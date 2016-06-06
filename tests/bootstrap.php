91ded3900cfea7f410f181b25a084ac39cc2f97a
Sathish <meet.sathish@gmail.com>
2016-06-06 15:41:46 +0400

    final moment fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * author:sathish
+ * desc: bootsrap lib directory
+ * copyright:nil
  */
 
 include_once('AutoLoader.php');
